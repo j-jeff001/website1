a17b04d9c8ba02b3e3ea8b1d185713be295ac309
John Jeffery <142925227+j-jeff001@users.noreply.github.com>
2024-03-12 09:53:35 -0700

    First Commit

---
+++ website1/js/expanding.js
@@ -0,0 +1,17 @@
+panels = document.querySelectorAll('.panel')
+
+
+console.log("HELLO")
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
