107f38d31b2091da1b3a16dce8cad59afeb2663f
John Jeffery <142925227+j-jeff001@users.noreply.github.com>
2024-03-15 16:23:51 +0000

    Fri, Mar 15, 2024, 9:23 AM -07:00

--- js/loading.js
+++ js/loading.js
@@ -12,7 +12,7 @@ function blurring() {
         clearInterval(int)
     }
     loadText.innerText = `${load}%`
-    loadText.style.opacity = 1 - load/100
+    loadText.style.opacity = 1 - load / 100
 
     bg.style.filter = `blur(${30-(load/100)*30}px)`
 }
