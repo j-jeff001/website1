f50fa6590983cfcafed2a7a1d07cfa322c242f5f
John Jeffery <142925227+j-jeff001@users.noreply.github.com>
2024-03-17 14:11:26 -0700

    Update sound.js

---
+++ js/sound.js
@@ -0,0 +1,22 @@
+sounds = ['hey_thats', 'look_at', 'metal', 'obamna', 'ohmygod', 'wow' ]
+
+sounds.forEach(sound => {
+    btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText=sound
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    }) 
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        song = document.getElementById(sound)
+        song.pause()
+        song.currentTime = 0
+    })
+}
