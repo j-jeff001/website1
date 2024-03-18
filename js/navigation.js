16985d6612f347555207d92767d367eb772389c1
John Jeffery <142925227+j-jeff001@users.noreply.github.com>
2024-03-17 20:00:30 -0700

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,9 @@
+nav = document.querySelector('.nav')
 
+window.addEventListener('scroll', fixNav) {
+  console.log(window.scrollY)
+  if (window.scrollY > 223) {
+    nav.classList.add('active') } else {
+    nav.classList.remove('active')
+  }
+}
